c5b8109595f171d8648436d0d2a0aa4cb838be0b
SonicBSV <sonicbsv@ya.ru>
2024-10-05 21:01:49 +0300

    sdm660-common: add ZenParts

--- sdm660.mk
+++ sdm660.mk
@@ -490,3 +490,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(COMMON_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# ZenParts
+PRODUCT_PACKAGES += \
+    ZenParts
+
