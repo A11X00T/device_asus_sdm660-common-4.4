eb53487f061b71e0a41e7816333804b290540c19
SonicBSV <sonicbsv@ya.ru>
2024-10-05 21:39:13 +0300

    sdm660-common: drop TetheringConfigOverlay * deprecated

--- sdm660.mk
+++ sdm660.mk
@@ -479,7 +479,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
     libwifi-hal-qcom \
-    TetheringConfigOverlay \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
